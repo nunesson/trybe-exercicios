71917194448aafa742d0d744993422267155dbd5
Anderson Nunes <quatroovos@gmail.com>
2022-06-22 14:41:34 -0300

    Primeir commit, exercicios de Array

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica/dia2-4-array-e-loop-for/array1.js
@@ -0,0 +1,38 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu.indexOf('Portfólio');
+
+// console.log(menuServices);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+
+console.log(menu);
