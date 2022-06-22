8019f8b4ec3c3b1024997016065af14511242ade
Anderson Nunes <quatroovos@gmail.com>
2022-06-22 08:51:45 -0300

    Fazendo exercício maior de 3

--- fundamentos/bloco-4-introducao-javascript-e-logica/dia1-4-primeiros-passos/exercicio-maior-de-3.js
+++ fundamentos/bloco-4-introducao-javascript-e-logica/dia1-4-primeiros-passos/exercicio-maior-de-3.js
@@ -1,10 +1,16 @@
-const num1 = 25;
-const num2 = 23;
-const num3 = 21;
+const num1 = 39;
+const num2 = 39;
+const num3 = 39;
 
-if (num1 > num2) {
+if (num1 > num2 && num1 > num3) {
     console.log("O número maior é o 1");
 }
-else{
+else if (num2 > num1 && num2 > num3) {
     console.log("O número maior é o 2");
+}
+else if (num3 > num1 && num3 > num2) {
+    console.log("O número maior é o 3")
+}
+else {
+    console.log ("Os números são iguais")
 }
