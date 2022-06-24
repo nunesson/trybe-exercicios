5998596054b7c83e9b8b2bc219c94726f8a523b4
Anderson Nunes <quatroovos@gmail.com>
2022-06-24 15:29:46 -0300

    Fazendo exercício objetos e funções carro

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica/dia4-4-objetos-e-funcoes/objetos-e-funcoes-1.js
@@ -0,0 +1,9 @@
+let car = {
+  type: 'Peugeot',
+  model: '3008',
+  color: 'Cinza',
+};
+
+console.log(`O meu carro vai ser um ${car.type} ${car.model} ${car.color}`); //template string
+
+console.log('O meu carro vai ser um ' + car.type + ' ' + car.model + ' ' + car.color); //template noob
