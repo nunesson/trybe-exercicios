a0c976b80aaeafdce0554e1187cc4da5881d4fce
Anderson Nunes <quatroovos@gmail.com>
2022-07-21 10:37:24 -0300

    exemplos HOF2

---
+++ fundamentos/bloco-8-HOFs/exercicios-8.1/exemplo-HOF-4.js
@@ -0,0 +1,5 @@
+const numberGenerator = () => {
+  return Math.random() * 60;
+}
+
+console.log(Math.round(numberGenerator()));
---
+++ fundamentos/bloco-8-HOFs/exercicios-8.1/exemplo-HOF-5-video.js
@@ -0,0 +1,7 @@
+const sum = (num1, num2) => num1 + num2;
+
+const sub = (num1, num2) => num1 - num2;
+
+const calculator = (func) => func(10, 5);
+
+console.log(calculator(sum));
