33f7e4589b63f126b91b558fe404e7ca31413127
Anderson Nunes <quatroovos@gmail.com>
2022-08-17 15:17:17 -0300

    Exemplo React Gatinho

---
+++ front-end/react/exercicios-10.2/fixation-exercises-10-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import staringCat from './staringCat.jpg'
+import Image from './Image';
+
+function App() {
+  return (
+    <main>
+      <Image source={staringCat} alternativeText="Cute cat staring" />
+    </main>
+  );
+}
+
+export default App;
