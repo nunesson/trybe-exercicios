9fb789c17064f5dbb43e0e6e22e17112886fbc1f
nunesson <quatroovos@gmail.com>
2022-09-22 14:33:31 -0300

    exercicio 1 redux

---
+++ redux/exemplo1-redux/exemplo1-redux/index.js
@@ -0,0 +1,34 @@
+const Redux = require('redux');
+
+const fazerLogin = (email) => ({
+  type: 'LOGIN',
+  email
+});
+
+const ESTADO_INICIAL = { // como se fosse prevState
+  login: false,
+  email: '',
+};
+
+const reducer = (state = ESTADO_INICIAL, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        login: !state.login,
+        email: action.email
+      };
+
+    default: // No switch, sempre precisamos ter um caso default!
+      return state;
+  }
+  };
+  
+  const store = Redux.createStore(reducer);
+
+  store.dispatch(fazerLogin('nunesson@gmail.com'))
+
+  console.log(store); 
+  console.log(store.getState());
+  
